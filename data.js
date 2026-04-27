b09bcb64e3ee1b4044683dfd795a73183369f6d0
aesopn04104 <7dgtzpdryf@privaterelay.appleid.com>
2026-04-27 00:33:17 -0600

    Add equipment pool and inventory

--- data.js
+++ data.js
@@ -44,9 +44,33 @@ const defaultEnemy = {
     lastAction: null
 };
 
+const equipmentPool = [
+    {
+        name: "改良型步槍瞄準器",
+        type: "武器配件",
+        description: "提升射擊穩定性的瞄準模組。"
+    },
+    {
+        name: "強化關節護板",
+        type: "防護裝備",
+        description: "可降低關節部位受損風險的外掛護板。"
+    },
+    {
+        name: "輕量化推進噴口",
+        type: "推進裝備",
+        description: "提高短距離機動反應的推進組件。"
+    },
+    {
+        name: "備用感測器組件",
+        type: "感測裝備",
+        description: "可用於替換受損雷達與光學輔助模組。"
+    }
+];
+
 let player = {};
 let allies = [];
 let enemies = [];
+let inventory = [];
 let battleOver = false;
 let turn = 1;
 let resources = 50;
